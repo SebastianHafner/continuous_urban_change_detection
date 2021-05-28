845ac347e93b22e904bf3e65be2a673affc69080
SebastianHafner <hafnersailing@gmail.com>
2021-05-28 02:48:39 +0200

    more workflow plots

--- workflow_functions.py
+++ workflow_functions.py
@@ -47,15 +47,76 @@ def export_probability_cube(dataset: str, aoi_id: str):
 
 
 def plot_constant_function_fit(dataset: str, aoi_id: str, pixel_coords: tuple):
-    pass
+
+    fig, ax = plt.subplots(1, 1, figsize=(10, 4))
+
+    probs_cube = prediction_helpers.load_prediction_timeseries(dataset, aoi_id, dataset_helpers.include_masked())
+    m, n = pixel_coords
+    ts = probs_cube[m, n, ]
+    ax.scatter(np.arange(len(ts)), ts, c='k')
+
+    mean = np.mean(ts)
+    ax.plot((0, len(ts)-1), (mean, mean), label='$f_{c}(ts)$')
+
+    y_ticks = np.linspace(0, 1, 6)
+    ax.set_yticks(y_ticks)
+    ax.set_yticklabels([f'{y_tick:.1f}' for y_tick in y_ticks], fontsize=16)
+    ax.set_ylim((0, 1))
+    ax.set_xlabel('$\it{time}$', fontsize=16)
+    ax.set_xticks([])
+    ax.set_ylabel('BUA probability', fontsize=16)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.plot((1), (0), ls="", marker=">", ms=10, color="k",
+            transform=ax.get_yaxis_transform(), clip_on=False)
+    ax.legend(fontsize=16, loc='lower right', frameon=False)
+    plt.show()
 
 
-def plot_piecewise_constant_function_fit(dataset: str, aoi_id: str, pixel_coords: tuple):
-    pass
+def plot_piecewise_constant_function_fit(dataset: str, aoi_id: str, pixel_coords: tuple, t_values: list):
+    fig, ax = plt.subplots(1, 1, figsize=(10, 4))
+    fs = 18
+    probs_cube = prediction_helpers.load_prediction_timeseries(dataset, aoi_id, dataset_helpers.include_masked())
+    m, n = pixel_coords
+    ts = probs_cube[m, n, ]
+    ax.scatter(np.arange(len(ts)), ts, c='k')
+
+    mean = np.mean(ts)
+    ax.plot((0, len(ts)-1), (mean, mean), label='$f_{0}(x)$', c='k')
+
+    cmap = visualization.DateColorMap(len(ts)).get_cmap()
+
+    for t in t_values:
+        mean_presegment = np.mean(ts[:t])
+        mean_postsegment = np.mean(ts[t:])
+        ts_fitted = t * [mean_presegment] + (len(ts)-t) * [mean_postsegment]
+        x_values = np.arange(len(ts))
+
+        ts_fitted = 2 * [mean_presegment] + 2 * [mean_postsegment]
+        x_values = [0, t - 1 + 0.49, t - 1 + 0.51, len(ts) - 1]
+        ax.plot(x_values, ts_fitted, label=f'$f_{{{t}}}(x)$', c=cmap(t))
+
+    y_ticks = np.linspace(0, 1, 6)
+    ax.set_yticks(y_ticks)
+    ax.set_yticklabels([f'{y_tick:.1f}' for y_tick in y_ticks], fontsize=fs)
+    ax.set_ylim((0, 1))
+    ax.set_xlabel('$\it{time}$', fontsize=fs)
+    ax.set_xticks([])
+    ax.set_ylabel('BUA probability', fontsize=fs)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.plot((1), (0), ls="", marker=">", ms=10, color="k",
+            transform=ax.get_yaxis_transform(), clip_on=False)
+    ax.legend(fontsize=fs, loc='lower right', frameon=False, ncol=2)
+    plt.show()
 
 
 if __name__ == '__main__':
     ds = 'spacenet7'
     aoi_id = 'L15-0358E-1220N_1433_3310_13'
+    pixel = (288, 206)
     # produce_workflow_data(ds, aoi_id)
-    export_probability_cube(ds, aoi_id)
+    # export_probability_cube(ds, aoi_id)
+
+    # plot_constant_function_fit(ds, aoi_id, pixel)
+    plot_piecewise_constant_function_fit(ds, aoi_id, pixel, [4, 10, 16])
